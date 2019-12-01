c91883a62fd6c9439e43f36c95f8fc6f6b28b2e6
Anton Herzog <info@antonherzog.com>
2019-12-01 18:15:42 +0100

    Example layouts for problem overview

--- src/BoleroPso.Client/Main.fs
+++ src/BoleroPso.Client/Main.fs
@@ -26,23 +26,51 @@ let update message model =
     match message with
     | Ping -> model
 
+let tableRow problem =
+    tr [] [
+        td [] [text problem.Description]
+        td [] [textf "%i" problem.Dimension]
+        td [] [textf "%A" problem.InputRange]
+        td [] [textf "%f" problem.MaxVelocity]
+    ]
+
+let tableLayout problems=
+    table [attr.``class`` "table"] [
+        thead [] [
+            th [] [text "Description"]
+            th [] [text "Dimension"]
+            th [] [text "Input Range"]
+            th [] [text "Maximum Velocity"]
+        ]
+        tbody [] [
+            forEach problems <| tableRow
+        ]
+    ]
+
 let codeCol o =
     div [attr.``class`` "column"] [
         code [][textf "%A" o]
     ]
 
+let columnLayout problems =
+    forEach problems <| fun p ->
+        div [attr.``class`` "columns"] [
+                p.Description |> codeCol
+                p.Dimension |> codeCol
+                p.Func |> codeCol
+                p.InputRange |> codeCol
+                p.MaxVelocity |> codeCol
+            ]
+
 let view model dispatch =
     div[] [
         h1 [] [text "Particle Swarm Optimizer"]
         hr []
-        forEach model.Functions <| fun f ->
-            div [attr.``class`` "columns"] [
-                    f.Description |> codeCol
-                    f.Dimension |> codeCol
-                    f.Func |> codeCol
-                    f.InputRange |> codeCol
-                    f.MaxVelocity |> codeCol
-                ]
+        h2 [] [text "Column Layout"]
+        columnLayout model.Functions
+        hr []
+        h2 [] [text "Table Layout"]
+        tableLayout model.Functions
     ]
 
 type MyApp() =
