ebfce054dd52cccdd4d88a8258438aa6fc7eebd5
Anton Herzog <info@antonherzog.com>
2019-12-08 14:04:05 +0100

    Working selection table for predined problems

--- src/BoleroPso.Client/Main.fs
+++ src/BoleroPso.Client/Main.fs
@@ -21,56 +21,52 @@ let initModel =
 
 type Message =
     | Ping
+    | SelectProblem of OptimizationProblem
 
 let update message model =
     match message with
     | Ping -> model
+    | SelectProblem p -> {model with SelectedFunction = p}
 
-let tableRow problem =
+let problemSelector model problem dispatch =
+    label [attr.``class`` "radio"][
+        input [
+            attr.``type`` "radio"
+            attr.``name`` "answer"
+            bind.checked (problem.Description=model.SelectedFunction.Description) (fun c -> dispatch (SelectProblem problem))
+        ]
+    ]
+
+let tableRow model dispatch problem =
     tr [] [
+        td [] [problemSelector model problem dispatch]
         td [] [text problem.Description]
         td [] [textf "%i" problem.Dimension]
         td [] [textf "%A" problem.InputRange]
         td [] [textf "%f" problem.MaxVelocity]
     ]
 
-let tableLayout problems=
+let tableLayout model dispatch=
     table [attr.``class`` "table"] [
         thead [] [
+            th [] [text "Selected"]
             th [] [text "Description"]
             th [] [text "Dimension"]
             th [] [text "Input Range"]
             th [] [text "Maximum Velocity"]
         ]
         tbody [] [
-            forEach problems <| tableRow
+            forEach model.Functions <| (tableRow model dispatch)
         ]
     ]
 
-let codeCol o =
-    div [attr.``class`` "column"] [
-        code [][textf "%A" o]
-    ]
-
-let columnLayout problems =
-    forEach problems <| fun p ->
-        div [attr.``class`` "columns"] [
-                p.Description |> codeCol
-                p.Dimension |> codeCol
-                p.Func |> codeCol
-                p.InputRange |> codeCol
-                p.MaxVelocity |> codeCol
-            ]
 
 let view model dispatch =
     div[] [
         h1 [] [text "Particle Swarm Optimizer"]
         hr []
-        h2 [] [text "Column Layout"]
-        columnLayout model.Functions
-        hr []
-        h2 [] [text "Table Layout"]
-        tableLayout model.Functions
+        h2 [] [text "Predifined Problems"]
+        tableLayout model dispatch
     ]
 
 type MyApp() =
