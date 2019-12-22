761fe9e24ded011186f1ade039012a1b05b5788a
Anton Herzog <info@antonherzog.com>
2019-12-22 13:40:03 +0100

    Add copy of sequential optimizer for starters

---
+++ src/BoleroPso.Client/SmartTerminationOptimizer.fs
@@ -0,0 +1,50 @@
+module BoleroPso.Client.SmartTerminationOptimizer
+
+open PSO
+open System
+
+let solve (problem : OptimizationProblem) : Solution =
+    Array.empty, 0.
+
+type Config = {
+    MaxIterations : int
+}
+
+
+let solve2 problem config (log: Log): Solution =
+    let logLabeled tag msg = log (sprintf "%s | %s" problem.Description tag) msg 
+
+    let startTime = DateTime.Now
+
+    let iterParticleOnProblem = problem |> Particle.itterate
+
+    let updateSingleParticle particle {GlobalBest = currentBest} = iterParticleOnProblem currentBest particle  
+
+    let updateSingleAndApplyToSwarm swarm particle =
+        let updatedSingleParticle = particle |> updateSingleParticle <| swarm
+        let updatedSwarm = Swarm.update swarm updatedSingleParticle.LocalBest
+        { updatedSwarm with Particles = (updatedSingleParticle::swarm.Particles) }
+
+    let singleIterationOverWholeSwarm ({GlobalBest = globalBest; Particles = particles}) : Swarm = 
+        ({GlobalBest = globalBest; Particles = List.empty}, particles) ||> Seq.fold  updateSingleAndApplyToSwarm
+      
+    let itterationWithIndex swarm i = 
+        let updatedSwarm = singleIterationOverWholeSwarm swarm
+      
+        let avgVelocity = updatedSwarm.Particles |> List.averageBy (fun p -> (Array.averageBy abs p.Velocity))
+        logLabeled (sprintf "average Velocity (%d)" i) (sprintf "%A" avgVelocity)
+      
+        updatedSwarm
+      
+    let initialSwarm = Swarm.create problem
+
+    let swarmAfterMaxIterations = (initialSwarm, [1 .. config.MaxIterations]) ||> Seq.fold itterationWithIndex 
+
+    let endTime = DateTime.Now
+    let duration = (endTime - startTime).TotalSeconds
+
+
+    logLabeled (sprintf "duration for %d iterations" config.MaxIterations) (sprintf "%A" duration)
+    logLabeled "final Positions" (sprintf "%A" swarmAfterMaxIterations.Particles)
+
+    swarmAfterMaxIterations.GlobalBest
