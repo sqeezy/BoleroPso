cedd46bdc0fc2c52b1016363ac79abec6b12cb14
Anton Herzog <info@antonherzog.com>
2020-01-25 16:21:55 +0100

    Working pulumi deployment to azure.

--- infra/Program.fs
+++ infra/Program.fs
@@ -1,23 +1,53 @@
 ﻿module Program
 
-open Pulumi.FSharp
+open Pulumi.Azure.AppService
+open Pulumi.Azure.AppService.Inputs
 open Pulumi.Azure.Core
-open Pulumi.Azure.Storage
+open Pulumi.FSharp
+open FSharp.Core
 
 let infra () =
     // Create an Azure Resource Group
-    let resourceGroup = ResourceGroup "resourceGroup"
+    let resourceGroup = ResourceGroup "BoleroPso"
 
-    // Create an Azure Storage Account
-    let storageAccount =
-        Account("storage",
-            AccountArgs
-               (ResourceGroupName = io resourceGroup.Name,
-                AccountReplicationType = input "LRS",
-                AccountTier = input "Standard"))
+    let sku = PlanSkuArgs
+                (Tier = input "Basic",
+                 Size = input "B1")
+    let plan =
+        Plan("BoleroDevPlan2",
+                PlanArgs
+                    (ResourceGroupName = io resourceGroup.Name,
+                     Kind = input "Linux",
+                     Sku = input sku,
+                     Reserved = input true
+                     ))
+    let image = "sqeezy/bolero-pso:latest"
+        
+    let appSettings = [
+                             "WEBSITES_ENABLE_APP_SERVICE_STORAGE", input "false"
+                      ]
+
+    let siteConfig =
+        AppServiceSiteConfigArgs(AlwaysOn = input true,
+                                 LinuxFxVersion = input (sprintf "DOCKER|%s" image))
+
+    let app =
+        AppService("BoleroDevPulumi",
+                    AppServiceArgs
+                        (
+                            ResourceGroupName = io resourceGroup.Name,
+                            AppServicePlanId = io plan.Id,
+                            AppSettings = inputMap appSettings,
+                            HttpsOnly = input true,
+                            SiteConfig = input siteConfig
+                        ))
 
     // Export the connection string for the storage account
-    dict [("connectionString", storageAccount.PrimaryConnectionString :> obj)]
+    [
+        ("name", app.Name :> obj);
+        ("url" , app.DefaultSiteHostname :> obj);
+    ]
+    |> dict
 
 [<EntryPoint>]
 let main _ =
