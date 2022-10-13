24918937ae9c03740b88859cecd0e77489fb550b
Anton Herzog <anton.herzog@afmg.eu>
2022-10-13 06:03:58 +0000

    Run actual build script now.

--- .space.kts
+++ .space.kts
@@ -4,6 +4,13 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Run shell script") {
+    container(displayName = "Build", image = "mcr.microsoft.com/dotnet/sdk:6.0") {
+        shellScript {
+            interpreter = "/bin/pwsh"
+            content = """
+                .\build.ps1
+            """
+        }
+    }
 }
