0e44582d1d2ff56b3e90d37721bde1330c64bee5
Anton Herzog <info@antonherzog.com>
2022-10-16 09:33:13 +0200

    Switch container image to another ubuntu version

--- .space.kts
+++ .space.kts
@@ -5,7 +5,7 @@
 */
 
 job("Run shell script") {
-    container(displayName = "Build", image = "mcr.microsoft.com/dotnet/sdk:6.0-windowsservercore-ltsc2022") {
+    container(displayName = "Build", image = "mcr.microsoft.com/dotnet/sdk:6.0-jammy") {
         shellScript {
             interpreter = "/bin/bash"
             content = """
